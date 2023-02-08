b145089e9810ba81dbab58ce5aeb8d6149ac070c
Palash Bauri <palashbauri1@gmail.com>
2023-02-08 19:14:23 +0530

    impl display for ast

--- src/ast.rs
+++ src/ast.rs
@@ -1,4 +1,4 @@
-use std::rc::Rc;
+use std::{fmt::Display, rc::Rc};
 
 use crate::token::{NumberToken, Token};
 
@@ -18,6 +18,22 @@ pub struct Program {
 pub struct Identifier {
     pub token: Token,
     pub name: String,
+}
+
+impl Display for Program {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut res = String::new();
+        for s in &self.stmts {
+            res.push_str(format!("{};", s).as_str())
+        }
+        write!(f, "PROG[{}]", res)
+    }
+}
+
+impl Display for Identifier {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "id<{}>", self.name)
+    }
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -95,6 +111,111 @@ pub enum Expr {
     ErrExpr,
 }
 
+impl Display for Expr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let result: String = match self {
+            Expr::NumExpr {
+                token: _,
+                value,
+                is_int: _,
+            } => match value {
+                NumberToken::Int(i) => format!("({})", i),
+                NumberToken::Float(f) => format!("({})", f),
+            },
+            Expr::IdentExpr { token: _, value } => format!("ident({})", value),
+            Expr::BoolExpr { token: _, value } => format!("bool({})", value),
+            Expr::StringExpr { token: _, value } => format!("str({})", value),
+            Expr::Break { token: _, value: _ } => format!("break()"),
+            Expr::PrefixExpr {
+                token: _,
+                op,
+                right,
+            } => format!("pre({}{})", op.literal, right),
+            Expr::InfixExpr {
+                token: _,
+                left,
+                op,
+                right,
+            } => format!("inf({}{}{})", left, op.literal, right),
+            Expr::ArrayExpr { token: _, elems } => {
+                let mut arrs: String = String::new();
+                for e in elems {
+                    arrs.push_str(format!("{}", e).as_str());
+                }
+                format!("arr({})", arrs)
+            }
+            Expr::IndexExpr {
+                token: _,
+                left,
+                index,
+            } => {
+                format!("index({}:{})", left, index)
+            }
+            Expr::IfExpr {
+                token: _,
+                cond,
+                trueblock,
+                elseblock,
+            } => {
+                if let Some(eb) = elseblock {
+                    format!("if({}:{}:{})", cond, trueblock, eb)
+                } else {
+                    format!("if({}:{})", cond, trueblock)
+                }
+            }
+
+            Self::WhileExpr {
+                token: _,
+                cond,
+                stmts,
+            } => {
+                format!("while({}:{})", cond, stmts)
+            }
+
+            Self::FuncExpr {
+                token: _,
+                params,
+                body,
+            } => {
+                let mut ps = String::new();
+
+                for p in params.to_vec() {
+                    ps.push_str(format!("{}", p).as_str())
+                }
+
+                format!("func({}:{})", ps, body)
+            }
+
+            Self::CallExpr {
+                token: _,
+                func,
+                args,
+            } => {
+                let mut ar = String::new();
+                for a in args {
+                    ar.push_str(format!("{},", a).as_str());
+                }
+
+                format!("call({}:{})", func, ar)
+            }
+
+            Self::HashExpr { token: _, pairs } => {
+                let mut hp = String::new();
+
+                for (a, b) in pairs {
+                    hp.push_str(format!("[{}:{}]", a, b).as_str());
+                }
+
+                format!("hash({})", hp)
+            }
+
+            Self::ErrExpr => "err()".to_string(),
+        };
+
+        write!(f, "{}", result)
+    }
+}
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum Stmt {
     LetStmt {
@@ -127,4 +248,49 @@ pub enum Stmt {
         token: Token,
         expr: Rc<Expr>,
     },
+}
+
+impl Display for Stmt {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let result: String = match self {
+            Stmt::LetStmt {
+                token: _,
+                name,
+                value,
+            } => {
+                format!("let<{}:{}>", name, value)
+            }
+            Self::ReturnStmt { token: _, rval } => {
+                format!("ret<{}>", rval)
+            }
+
+            Self::ShowStmt { token: _, value } => {
+                let mut res = String::new();
+                for v in value {
+                    res.push_str(format!("{},", v).as_str())
+                }
+                format!("show<{}>", res)
+            }
+
+            Self::IncludeStmt { token: _, filename } => {
+                format!("inc<{}>", filename)
+            }
+
+            Stmt::BlockStmt { token: _, stmts } => {
+                let mut res = String::new();
+
+                for s in stmts {
+                    res.push_str(format!("{};", s).as_str());
+                }
+
+                format!("blk<{}>", res)
+            }
+
+            Self::ExprStmt { token: _, expr } => {
+                format!("{}", expr)
+            }
+        };
+
+        write!(f, "{}", result)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use pras::{lexer::Lexer, parser::Parser};
 
 fn main() {
     //le&t mut a = Lexer::new("1+2-1100 hello পলাশ");
-    let a = Lexer::new("let x = ekti kaj(a,b) 1 + 2 sesh");
+    let a = Lexer::new("1+2*3/4");
     //while !a.is_at_eof() {
     //    println!("{:?}", a.next_token());
     //}
@@ -14,6 +14,6 @@ fn main() {
             println!("ERR=>{}", err.msg);
         }
     } else {
-        println!("{pp:#?}")
+        println!("{}", pp)
     }
 }
